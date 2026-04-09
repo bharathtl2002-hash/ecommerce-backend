2d4b6e9f8e332e4e50609595f4fe3963709d987f
bharathtl2002-hash <bharathtl2002@gmail.com>
2026-04-10 00:26:36 +0530

    fix allowed hosts

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3*o8sf9#%fwdb93a0yw=344f7-l1ys6k0b+-%21waa#l88xy$w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
